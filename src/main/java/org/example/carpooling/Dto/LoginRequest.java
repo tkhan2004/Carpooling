55fee616b69cf51454c21ba956d1c711c74c74ba
khangnguyen <tcag24042004@gmail.com>
2025-08-09 14:27:42 +0700

    refactor : using lombok for dto and

--- src/main/java/org/example/carpooling/Dto/LoginRequest.java
+++ src/main/java/org/example/carpooling/Dto/LoginRequest.java
@@ -1,29 +1,13 @@
 package org.example.carpooling.Dto;
 
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String email;
 
-    public LoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
     private String password;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
