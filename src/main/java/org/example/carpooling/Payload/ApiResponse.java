63b7949932054344bd494126ec6cf6e06921591e
khangnguyen <tcag24042004@gmail.com>
2025-08-11 16:20:58 +0700

    fix : add constructor

--- src/main/java/org/example/carpooling/Payload/ApiResponse.java
+++ src/main/java/org/example/carpooling/Payload/ApiResponse.java
@@ -1,38 +1,27 @@
 package org.example.carpooling.Payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+
 public class ApiResponse<T> {
     private boolean isSuccess;
     private String message;
-
-    public boolean isSuccess() {
-        return isSuccess;
-    }
-
-    public void setSuccess(boolean success) {
-        isSuccess = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
 
     public ApiResponse(boolean isSuccess, String message, T data) {
         this.isSuccess = isSuccess;
         this.message = message;
         this.data = data;
     }
+
+    public ApiResponse(boolean isSuccess, String message) {
+        this.isSuccess = isSuccess;
+        this.message = message;
+    }
+
+
 
     private T data;
 }
