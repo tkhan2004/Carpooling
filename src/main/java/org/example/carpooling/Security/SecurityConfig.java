01ae69bab7a8f04d9e991614def37dad599690b9
khangnguyen <tcag24042004@gmail.com>
2025-08-23 22:10:44 +0700

    fix : change redis config

--- src/main/java/org/example/carpooling/Security/SecurityConfig.java
+++ src/main/java/org/example/carpooling/Security/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // IMPORTANT: Specify the exact origin of your frontend
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500","http://127.0.0.1:5502", "http://localhost:5500","http://localhost:55668","http://localhost:63342"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500","http://127.0.0.1:5502", "http://localhost:5500","http://localhost:55668","http://localhost:63342", "https://carpooling-j5xn.onrender.com" ));
         // Specify allowed methods (including OPTIONS for preflight)
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "X-Requested-With"));
