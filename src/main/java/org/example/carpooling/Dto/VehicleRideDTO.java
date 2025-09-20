05568903670877eede7c53bc743bfa6d48012cab
khangnguyen <tcag24042004@gmail.com>
2025-09-20 23:25:01 +0700

    feat: add vehicleDTO

--- src/main/java/org/example/carpooling/Dto/VehicleRideDTO.java
+++ src/main/java/org/example/carpooling/Dto/VehicleRideDTO.java
@@ -5,14 +5,11 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class VehicleDTO {
+public class VehicleRideDTO {
     private String licensePlate;
     private String brand;
     private String model;
     private String color;
     private Integer numberOfSeats;
     private String vehicleImageUrl;
-    private String licenseImageUrl;
-    private String licenseImagePublicId;
-    private String vehicleImagePublicId;
 }
